34ef93fe1d21d8bb9767d40389f6216243e6656f
Jared A Hayes <jhayes907@gmail.com>
2022-12-05 21:19:27 -0600

    Feat: built out tests for questions

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,10 +1,10 @@
-# from django.http import Http404
+from django.http import Http404
 # from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-# from django.template import loader
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -15,11 +15,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls_app/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
    
 
 class ResultsView(generic.DetailView):
